4cd175ff4217b8b34cdcb6867f01417f103a0c11
bormoley-ua <zhuravel_leonid@yahoo.com>
2023-01-03 10:52:25 +0200

    mongodb logic

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,87 +1,214 @@
-const fs = require('fs');
+//const fs = require('fs');
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+// );
+// exports.checkID = (req, resp, next, val) => {
+//   console.log(`tour id is: ${val}`);
+// exports.checkBody = (req, resp, next) => {
+//   console.log(`tour Name is: ${req.body.name} and Price is: ${req.body.price}`);
+//   if (!req.body.name || !req.body.price) {
+//     return resp.status(400).json({
+//       status: 'fail. Bad request',
+//       message: 'Invalid Name or Price of rote',
+//     });
+//   }
+//   next();
+// };
+const { query } = require('express');
+const Tour = require('./../models/tourModel');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+// const testTour = new Tour({
+//   name: 'The park camper',
+//   price: 997,
+// });
 
-exports.checkID = (req, resp, next, val) => {
-  console.log(`tour id is: ${val}`);
+// testTour
+//   .save()
+//   .then((doc) => {
+//     console.log(doc);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
-  if (req.params.id * 1 > tours.length) {
-    return resp.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
+// if (req.params.id * 1 > tours.length) {
+//   return resp.status(404).json({
+//     status: 'fail',
+//     message: 'Invalid ID',
+//   });
+// }
+// next();
+// };
 
-exports.checkBody = (req, resp, next) => {
-  console.log(`tour Name is: ${req.body.name} and Price is: ${req.body.price}`);
+exports.getAllTours = async (req, resp) => {
+  try {
+    //build the query
+    //1a filtering
+    const queryObj = { ...req.query };
 
-  if (!req.body.name || !req.body.price) {
-    return resp.status(400).json({
-      status: 'fail. Bad request',
-      message: 'Invalid Name or Price of rote',
-    });
-  }
-  next();
-};
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
 
-exports.getAllTours = (req, resp) => {
-  resp.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
-  });
-};
+    excludedFields.forEach((el) => delete queryObj[el]);
 
-exports.getTourById = (req, resp) => {
-  console.log(req.params);
+    //1b advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-  const tour = tours.find((el) => el.id === req.params.id * 1);
+    //console.log(JSON.parse(queryStr));
+    //console.log(req.query, queryObj);
 
-  resp.status(200).json({
-    status: 'success',
-    RequestedAt: req.requestTime,
-    data: {
-      tour,
-    },
-  });
-};
+    let query = Tour.find(JSON.parse(queryStr));
 
-exports.insertTour = (req, resp) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      resp.status(201).json({
-        status: 'success',
-        data: {
-          newTour,
-        },
-      });
+    //2 sort
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query.sort(sortBy);
+      //sort('price ratingsAverage' )
+    } else {
+      query.sort('-createdAt');
     }
-  );
+
+    //3 field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    //4 pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+
+    //page=2&limit=10
+    query = query.skip((page - 1) * limit).limit(limit);
+
+    //execute query
+    const tours = await query;
+
+    // const tours = await Tour.find({
+    //   duration: 5,
+    //   difficulty: 'easy',
+    // });
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    //send responce
+    resp.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours: tours,
+      },
+    });
+  } catch (err) {
+    resp.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.updateTour = (req, resp) => {
-  resp.status(200).json({
-    status: 'success',
-    data: '<Updated tour>',
-  });
+exports.getTourById = async (req, resp) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    //     const tour = await Tour.findOne({_id: req.params.id}});
+
+    resp.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    resp.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+  //  console.log(req.params);
+  // const tour = tours.find((el) => el.id === req.params.id * 1);
+  // resp.status(200).json({
+  //   status: 'success',
+  //   RequestedAt: req.requestTime,
+  //   data: {
+  //     tour,
+  //   },
+  // });
 };
 
-exports.removeTour = (req, resp) => {
-  resp.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.insertTour = async (req, resp) => {
+  try {
+    // const newTour = new Tour({});
+    // newTour.save();
+
+    //  Tour.create({}).then();
+    const newTour = await Tour.create(req.body);
+    resp.status(201).json({
+      status: 'success',
+      data: {
+        newTour,
+      },
+    });
+  } catch (err) {
+    resp.status(400).json({
+      status: 'fail',
+      message: err, //'Invalid dataset', //err
+    });
+  }
+};
+// const newId = tours[tours.length - 1].id + 1;
+// const newTour = Object.assign({ id: newId }, req.body);
+// tours.push(newTour);
+// fs.writeFile(
+//   `${__dirname}/dev-data/data/tours-simple.json`,
+//   JSON.stringify(tours),
+//   (err) => {
+//     resp.status(201).json({
+//       status: 'success',
+//       data: {
+//         newTour,
+//       },
+//     });
+//   }
+// );
+// };
+
+exports.updateTour = async (req, resp) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    resp.status(200).json({
+      status: 'success',
+      data: { tour },
+    });
+  } catch (err) {
+    resp.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.removeTour = async (req, resp) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    resp.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    resp.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
--- server.js
+++ server.js
@@ -1,10 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-console.log(app.get('env'));
 //console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  //.connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
+    useNewUrlParser: true,
+    //useCreateIndex: true,
+    //useFindAndMoify: false,
+  })
+  .then(() => console.log('Connection Established!'))
+  .catch();
 
 const port = process.env.PORT || 3000;
 
